8ee9d28b83e3bd8bb2ecae02b5985875d31f2555
Filipe Torres <felipetorres2405@gmail.com>
2025-02-01 18:45:14 -0300

    melhorias na interface e adicionando uma pagina de ajuda

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('submitButton').addEventListener('click', function () {
-  const file1 = document.getElementById('following').files[0];
-  const file2 = document.getElementById('followers').files[0];
+  const following_file = document.getElementById('following').files[0];
+  const followers_file = document.getElementById('followers').files[0];
   const outputDiv = document.getElementById('output');
 
-  if (file1 && file2) {
+  if (following_file && followers_file) {
     outputDiv.innerHTML = '<p>Comparando arquivos...</p>';
 
     const extractInstagramLinks = (content) => {
@@ -11,8 +11,8 @@ document.getElementById('submitButton').addEventListener('click', function () {
       const doc = parser.parseFromString(content, 'text/html');
       const links = Array.from(doc.querySelectorAll('a'));
       return links
-        .map(link => link.href) // Extrai os href
-        .filter(href => href.startsWith('https://www.instagram.com/')); // Filtra URLs do Instagram
+        .map(link => link.href) 
+        .filter(href => href.startsWith('https://www.instagram.com/')); 
     };
 
     const processFile = (file) => {
@@ -27,25 +27,22 @@ document.getElementById('submitButton').addEventListener('click', function () {
       });
     };
 
-    // Processar os dois arquivos
-    Promise.all([processFile(file1), processFile(file2)]).then(([linksFile1, linksFile2]) => {
-      const setFile1 = new Set(linksFile1); // Conjunto de links do arquivo 1
-      const setFile2 = new Set(linksFile2); // Conjunto de links do arquivo 2
+    Promise.all([processFile(following_file), processFile(followers_file)]).then(([linksFollowing, linksFollowers]) => {
+      const setFollowing = new Set(linksFollowing); 
+      const setFollowers = new Set(linksFollowers);
 
-      // Interseção dos dois conjuntos
-      const commonLinks = [...setFile1].filter(link => !setFile2.has(link));
+      const commonLinks = [...setFollowing].filter(link => !setFollowers.has(link));
 
-      // Exibir os resultados
       if (commonLinks.length > 0) {
-        outputDiv.innerHTML = `<p>Encontrados ${commonLinks.length} links em comum:</p>`;
+        outputDiv.innerHTML = `<p>Encontrados ${commonLinks.length} perfis que não seguem de volta:</p>`;
         commonLinks.forEach(link => {
-          outputDiv.innerHTML += `<a target="_blank" href="${link}">${link}</a><br>`;
+          outputDiv.innerHTML += `<li><a target="_blank" href="${link}">${link.slice(26)}</a></li>`;
         });
       } else {
-        outputDiv.innerHTML = '<p>Nenhum link em comum foi encontrado.</p>';
+        outputDiv.innerHTML = '<p>Todos seguem de volta.</p>';
       }
     });
   } else {
-    outputDiv.innerHTML = '<p>Por favor, selecione os dois arquivos antes de clicar em "Comparar".</p>';
+    outputDiv.innerHTML = '<p>Por favor, selecione os dois arquivos antes de clicar em "Enviar".</p>';
   }
 });
